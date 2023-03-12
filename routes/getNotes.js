42749f0f3c5b24bad285b7c87583a39e14f0f7f0
Michael Mount <mount.photos@gmail.com>
2023-03-12 15:15:45 -0600

    added saveBtn to push user input to db

--- routes/getNotes.js
+++ routes/getNotes.js
@@ -9,4 +9,15 @@ notes.get("/", (req, res) => {
   readFromFile("./db/db.json").then((data) => res.json(JSON.parse(data)));
 });
 
+notes.post("/", (req, res) => {
+  const { title, text } = req.body;
+
+  const newNote = {
+    title,
+    text,
+  };
+
+  readAndAppend(newNote, "./db/db.json");
+});
+
 module.exports = notes;
