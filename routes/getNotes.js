bbb483e8b9e88306faac1e4f0a88b16777462fb9
Michael Mount <mount.photos@gmail.com>
2023-03-12 17:28:13 -0600

    Fixed bug with notes from list not being passed to render function

--- routes/getNotes.js
+++ routes/getNotes.js
@@ -1,4 +1,5 @@
 const notes = require("express").Router();
+const { v4: uuidv4 } = require("uuid");
 const {
   readFromFile,
   readAndAppend,
@@ -9,12 +10,25 @@ notes.get("/", (req, res) => {
   readFromFile("./db/db.json").then((data) => res.json(JSON.parse(data)));
 });
 
+notes.get("/:note_id", (req, res) => {
+  const noteId = req.params.note_id;
+  readFromFile("./db/db.json")
+    .then((data) => JSON.parse(data))
+    .then((json) => {
+      const getNote = json.filter((note) => note.id === noteId);
+      return getNote.legnth > 0
+        ? res.json(getNote)
+        : res.json("Failed to Find Tip");
+    });
+});
+
 notes.post("/", (req, res) => {
   const { title, text } = req.body;
 
   const newNote = {
     title,
     text,
+    id: uuidv4(),
   };
 
   readAndAppend(newNote, "./db/db.json");
