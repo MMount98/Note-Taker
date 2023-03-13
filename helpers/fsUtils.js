facf451f51541a056c0dc2b1a046df134f56cab1
Michael Mount <mount.photos@gmail.com>
2023-03-12 23:06:25 -0600

    Added comments to: routes, server.js, and helpers

--- helpers/fsUtils.js
+++ helpers/fsUtils.js
@@ -1,21 +1,34 @@
+//MODULES
 const fs = require("fs");
 const util = require("util");
 
-// Promise version of fs.readFile
+//Setting readForFile to Promise obj
 const readFromFile = util.promisify(fs.readFile);
 
-const writeToFile = (destination, content) =>
-  fs.writeFile(destination, JSON.stringify(content, null, 4), (err) =>
-    err ? console.error(err) : console.info(`\nData written to ${destination}`)
+/**
+ * Function will write to a passed JSON if passed data is a stringfied
+ * @param {string} file file that will be written too
+ * @param {object} value the append information
+ */
+
+const writeToFile = (file, value) =>
+  fs.writeFile(file, JSON.stringify(value, null, 4), (err) =>
+    err ? console.error(err) : console.info(`\nData written to ${file}`)
   );
 
-const readAndAppend = (content, file) => {
+/**
+ * Function will read a passed file and append a passed value to it
+ * @param {object} value The value you want to append
+ * @param {string} file The path to the desired file
+ */
+
+const readAndAppend = (value, file) => {
   fs.readFile(file, "utf8", (err, data) => {
     if (err) {
       console.error(err);
     } else {
       const parsedData = JSON.parse(data);
-      parsedData.push(content);
+      parsedData.push(value);
       writeToFile(file, parsedData);
     }
   });
--- routes/getNotes.js
+++ routes/getNotes.js
@@ -9,27 +9,34 @@ const {
 
 //ROUTES
 
-//GET
+//GET - this route is responsible for sending the database to front end to be displyed in saved noted
 notes.get("/", (req, res) => {
-  readFromFile("./db/db.json").then((data) => res.json(JSON.parse(data)));
+  readFromFile("./db/notes.json").then((data) => res.json(JSON.parse(data)));
 });
 
+//GET - this route is responsible for displaying the active Not
 notes.get("/:note_id", (req, res) => {
+  //noteId is set to the vaule of the requested note id
   const noteId = req.params.note_id;
-  readFromFile("./db/db.json")
+  //this function read the desired file from database
+  readFromFile("./db/notes.json")
+    //converting into object
     .then((data) => JSON.parse(data))
+    //set getNote to the a filter method that will read through given obj and return the match id if fails to find matching id it will return a string
     .then((json) => {
       const getNote = json.filter((note) => note.id === noteId);
       return getNote.legnth > 0
         ? res.json(getNote)
-        : res.json("Failed to Find Tip");
+        : res.json("Failed to Find Note");
     });
 });
 
-//POST
+//POST - this route is responsible for taking in user input and saving to database
 notes.post("/", (req, res) => {
+  //obj decontruction to pull disred key:value
   const { title, text } = req.body;
 
+  //constructs the new note obj from user input and sets a random value
   if (req.body) {
     const newNote = {
       title,
@@ -37,29 +44,32 @@ notes.post("/", (req, res) => {
       id: uuidv4(),
     };
 
-    readAndAppend(newNote, "./db/db.json");
+    //calls the readAndAppend function and pass the new obj and appends to disered file path
+    readAndAppend(newNote, "./db/notes.json");
     res.json(`Added '${newNote.title}' to Data Base!`);
   } else {
     res.error(`Issue adding '${newNote.title}' to Data Base!`);
   }
 });
 
-
-//DELETE
+//DELETE - this route is responsible for deleting a desired note/obj from database
 notes.delete("/:note_id", (req, res) => {
+  //noteId is set to the vaule of the requested note id
   const noteId = req.params.note_id;
-  console.log(noteId);
-  readFromFile("./db/db.json")
+
+  //calls readFromFile function with desired file
+  readFromFile("./db/notes.json")
+    //creates a obj from stringed file
     .then((data) => JSON.parse(data))
+    //newDn is set to filter through passed obj to remove the mathing obj
     .then((notes) => {
       const newDb = notes.filter((notes) => notes.id !== noteId);
-
-      writeToFile("./db/db.json", newDb);
-
+      //passes newly edited obj with a disred file path overwriting the last file
+      writeToFile("./db/notes.json", newDb);
+      //response with success
       res.json(`Note: Deleted`);
     });
 });
-
 
 //EXPROTS
 module.exports = notes;
